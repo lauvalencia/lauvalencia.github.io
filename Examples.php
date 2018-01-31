21bc6521eff8d6bbd83c448955a16a1548ecd317
lauvalencia <lauvalencia95@gmail.com>
2018-01-31 12:55:39 -0500

    save my work

---
+++ Examples.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  
+  <h1> Registro de Clientes </h1>
+
+ <form method = "POST" action = "ejm01.php" accept-charset = "UTF-8">
+
+  Nombre
+  <input type = "text" name = "txtnombre" size = "20" mazlenght = "20"><br> 
+
+  Email
+  <input type = "text" name = "txtmail" size = "20" mazlenght = "20"><br> 
+
+  <input type = "submit" value = "enviar">
+
+  <input type = "reset" value = "Limpiar">
+
+ </form>
+
+</html>
